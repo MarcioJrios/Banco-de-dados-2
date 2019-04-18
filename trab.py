3926bc03a1ee842d74a68c47b9fc5717051bda66
MarcioJrios <31110984+MarcioJrios@users.noreply.github.com>
2019-04-18 11:15:19 -0300

    trabalho sobre log

---
+++ trab.py
@@ -0,0 +1,13 @@
+import csv
+
+Redo = []
+Undo = []
+
+#arquivo = open(teste.txt, 'r')
+arquivo = []
+with open('teste.txt', newline='') as inputfile:
+    for row in csv.reader(inputfile):
+        arquivo.append(row)
+
+arquivo.reverse()
+print(arquivo)
